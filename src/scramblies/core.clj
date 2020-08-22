f89ce4e9db976ba4a0ea32f5fd46f0137b922007
NPException <npexception42@gmail.com>
2020-08-22 10:39:42 +0200

    refine `scramble?` implementation
    reduced function runtime by ~35%:
    - replaced on `frequencies` usage by explicitly looping over the required characters (str2)
    - use `transient` to make updating the frequencies map of available characters more efficient

--- src/scramblies/core.clj
+++ src/scramblies/core.clj
@@ -1,23 +1,17 @@
 (ns scramblies.core
   (:gen-class))
 
-(defn ^:private enough-chars?
-  [required-char-count [char n]]
-  (>= n (required-char-count char)))
-
 (defn scramble?
   "returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false"
   [str1 str2]
-  (let [required-char-counts (frequencies str2)
-        available-char-counts (select-keys
-                                (frequencies str1)
-                                (keys required-char-counts))]
-    (and
-      (= (count required-char-counts)
-         (count available-char-counts))
-      (every?
-        #(enough-chars? required-char-counts %)
-        available-char-counts))))
+  (loop [available-char-counts (transient (frequencies str1))
+         [c & remaining] str2]
+    (or (nil? c)
+        (if-let [n (available-char-counts c)]
+          (and (> n 0)
+               (recur
+                 (assoc! available-char-counts c (dec n))
+                 remaining))))))
 
 
 (defn -main
